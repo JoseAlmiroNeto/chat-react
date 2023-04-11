c174c70ea9053eb541f000f7031251b4e66c180a
José Almiro <josealmironettto@gmail.com>
2023-04-11 12:16:35 -0300

    first commit

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="697d7971-08f8-42d5-a25b-63f33909caff"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
